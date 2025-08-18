8322c315fa233a339deb8c89bf02d498a6e52581
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-08-18 17:31:41 +0000

    Release 1.6.0

--- src/Candid.Net/Candid.cs
+++ src/Candid.Net/Candid.cs
@@ -54,7 +54,7 @@ public partial class Candid
                 { "X-Fern-Language", "C#" },
                 { "X-Fern-SDK-Name", "Candid.Net" },
                 { "X-Fern-SDK-Version", Version.Current },
-                { "User-Agent", "Candid.Net/1.5.0" },
+                { "User-Agent", "Candid.Net/1.6.0" },
             }
         );
         clientOptions ??= new ClientOptions();
--- src/Candid.Net/Core/Public/Version.cs
+++ src/Candid.Net/Core/Public/Version.cs
@@ -2,5 +2,5 @@ namespace Candid.Net;
 
 internal class Version
 {
-    public const string Current = "1.5.0";
+    public const string Current = "1.6.0";
 }
